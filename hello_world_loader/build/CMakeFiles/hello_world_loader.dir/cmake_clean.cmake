583b1b45cfd01259f81391c4e2919d978a42ab26
ruben <rubenovidalo@gmail.com>
2024-09-03 13:13:23 -0400

    add

---
+++ hello_world_loader/build/CMakeFiles/hello_world_loader.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world_loader.dir/hello_world_loader.cc.o"
+  "CMakeFiles/hello_world_loader.dir/hello_world_loader.cc.o.d"
+  "hello_world_loader"
+  "hello_world_loader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world_loader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
